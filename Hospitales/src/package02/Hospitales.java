178fd5b0694507e43a4728aadfddbc1f16403c6c
ByronCast09 <byronjosue14z@gmail.com>
2022-05-20 19:34:39 -0500

    Taller06

---
+++ Hospitales/src/package02/Hospitales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package package02;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Hospitales {
+    private String ciudad;
+    private String provincia;
+    }
+    
+}
---
+++ Hospitales/src/package02/Medico.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package package02;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Medico {
+    private String nombredoc;
+    private String especialidad;
+    private double sueldoMes;
+}
